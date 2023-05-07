a5e31d3e87f7181d4d5909e200c58a15a2365aaa
sako <2511013095@qq.com>
2023-05-07 22:32:45 +0800

    首页列表显示基础功能

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -2,10 +2,62 @@
 from . import index_blue
 from ... import redis_store
 import logging
-from flask import current_app, render_template, session, jsonify
+from flask import current_app, render_template, session, jsonify, request
 
 from ...models import User, News, Category
 from ...utils.response_code import RET
+
+
+# 首页新闻列表
+# 请求路径: /newslist
+# 请求方式: GET
+# 请求参数: cid,page,per_page
+# 返回值: data数据
+@index_blue.route('/newslist')
+def newslist():
+    """
+    1. 获取参数
+    2. 参数类型转换
+    3. 分页查询
+    4. 获取到分页对象中的属性,总页数,当前页,当前页的对象列表
+    5. 将对象列表转成字典列表
+    6. 携带数据,返回响应
+    :return:
+    """
+    # 1. 获取参数
+    cid = request.args.get("cid", "1")
+    page = request.args.get("page", "1")
+    per_page = request.args.get("per_page", "10")
+
+    # 2. 参数类型转换
+    try:
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        page = 1
+        per_page = 10
+
+    # 3. 分页查询
+    try:
+        paginate = News.query.filter(News.category_id == cid).order_by(News.create_time.desc()).paginate(page, per_page,
+                                                                                                         False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="获取新闻失败")
+
+    # 4. 获取到分页对象中的属性,总页数,当前页,当前页的对象列表
+    totalPage = paginate.pages
+    currentPage = paginate.page
+    items = paginate.items
+
+    # 5. 将对象列表转成字典列表
+    news_list = []
+    for news in items:
+        news_list.append(news.to_dict())
+
+    # 6. 携带数据,返回响应
+    return jsonify(errno=RET.OK, errmsg="获取新闻成功", totalPage=totalPage, currentPage=currentPage,
+                   newsList=news_list)
 
 
 @index_blue.route('/', methods=['GET', 'POST'])
