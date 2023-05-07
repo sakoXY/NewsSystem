fe39eb4b87b43b4bfcb91c78cdb327ad09b7d57c
sako <2511013095@qq.com>
2023-05-07 15:24:35 +0800

    基本的短信验证码接口 实现短信可以在后台发送的功能 可以在终端得到短信的结果，在终端可以打印验证码，也无需yuntongxun来起到鉴别的作用

--- info/__init__.py
+++ info/__init__.py
@@ -39,7 +39,7 @@ def create_app(config_name):
     Session(app)
 
     # 使用CSRFProtect保护app
-    CSRFProtect(app)
+    # CSRFProtect(app)
 
     # 将首页蓝图index_blue，注册到app中
     from info.modules.index import index_blue
--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,8 +1,50 @@
-from flask import request, current_app, make_response
+import json
+
+from flask import request, current_app, make_response, jsonify
 
 from . import passport_blue
 from ... import redis_store, constants
+from ...libs.yuntongxun.sms import CCP
 from ...utils.captcha.captcha import captcha
+import re
+import random
+
+
+# 获取短信验证码
+# 请求路径: /passport/sms_code
+# 请求方式: POST
+# 请求参数: mobile, image_code,image_code_id
+# 返回值: errno, errmsg
+@passport_blue.route('/sms_code', methods=['POST'])
+def sms_code():
+    # 1. 获取参数
+    json_data = request.data
+    dict_data = json.loads(json_data)
+    mobile = dict_data.get("mobile")
+    image_code = dict_data.get("image_code")
+    image_code_id = dict_data.get("image_code_id")
+
+    # 2. 校验参数，图片验证码
+    # 从redis中取出图片验证码
+    redis_image_code = redis_store.get("image_code:%s" % image_code_id)
+
+    # 和传递的图片验证码比较
+    if (image_code != redis_image_code):
+        return jsonify(errno=10000, errmsg="图片验证码填写错误")
+
+    # 3. 校验参数，手机格式
+    if not re.match("1[3-9]\d{9}", mobile):
+        return jsonify(errno=20000, errmsg="手机格式不正确！")
+
+    # 4. 发送短信，调用封装好的ccp
+    ccp = CCP()
+    result = ccp.send_template_sms("13812341234", ['123123', 5], 1)
+
+    if result == -1:
+        return jsonify(errno=30000, errmsg="短信发送失败！")
+
+    # 5. 返回发送的状态
+    return jsonify(errno=0, errmsg="短信发送成功")
 
 
 # 功能：获取图片验证码
