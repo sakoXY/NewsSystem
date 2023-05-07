798d2b7d3c796f558fb6a2b91a39bc05fba4ee2c
sako <2511013095@qq.com>
2023-05-07 21:53:51 +0800

    热门新闻排行+2个已有数据库的导入

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -2,14 +2,14 @@
 from . import index_blue
 from ... import redis_store
 import logging
-from flask import current_app, render_template, session
+from flask import current_app, render_template, session, jsonify
 
-from ...models import User
+from ...models import User, News
+from ...utils.response_code import RET
 
 
-@index_blue.route('/')
+@index_blue.route('/', methods=['GET', 'POST'])
 def hello_world():
-
     # 1. 获取用户的登录信息
     user_id = session.get("user_id")
 
@@ -21,10 +21,23 @@ def hello_world():
         except Exception as e:
             current_app.logger.error(e)
 
+    # 3. 根据点击量，查询前10条新闻
+    try:
+        news = News.query.order_by(News.clicks.desc()).limit(10).all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="获取新闻失败")
+
+    # 4. 将新闻对象列表转成字典列表
+    news_list = []
+    for item in news:
+        news_list.append(item.to_dict())
+
     # 3. 拼接用户数据，渲染页面
     data = {
         # 如果user有值，返回左边的内容，否则返回右边的值
-        "user_info": user.to_dict() if user else ""
+        "user_info": user.to_dict() if user else "",
+        "news_list": news_list
     }
 
     return render_template("news/index.html", data=data)
