4f1afe970879149607a904f8f7b99a9a1182c5a6
sako <2511013095@qq.com>
2023-05-07 20:35:16 +0800

    右上角只显示用户登录了的显示界面or没登陆的注册按钮

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -2,35 +2,32 @@
 from . import index_blue
 from ... import redis_store
 import logging
-from flask import current_app, render_template
+from flask import current_app, render_template, session
+
+from ...models import User
 
 
 @index_blue.route('/')
 def hello_world():
-    # 测试redis存取数据
-    # redis_store.set("name", "xiayao")
-    # print(redis_store.get("name"))
 
-    # 测试Session存取
-    # session["name"] = "xxxy"
-    # print(session.get("name"))
+    # 1. 获取用户的登录信息
+    user_id = session.get("user_id")
 
-    # 没有继承日志之前，使用print输出，不方便做控制
-    # print("helloworld")
+    # 2. 通过user_id取出用户对象
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
 
-    # 使用日志记录方法logging进行输出可控
-    # logging.debug("输入调试信息")
-    # logging.info("输入详细信息")
-    # logging.warning("输入警告信息")
-    # logging.error("输入错误信息")
+    # 3. 拼接用户数据，渲染页面
+    data = {
+        # 如果user有值，返回左边的内容，否则返回右边的值
+        "user_info": user.to_dict() if user else ""
+    }
 
-    # 也可以使用current_app来输出日志信息，输出的时候有分割线，写在文件中完全一样
-    # current_app.logger.debug("输入调试信息")
-    # current_app.logger.info("输入详细信息")
-    # current_app.logger.warning("输入警告信息")
-    # current_app.logger.error("输入错误信息")
-
-    return render_template("news/index.html")
+    return render_template("news/index.html", data=data)
 
 
 # 处理网站logo
