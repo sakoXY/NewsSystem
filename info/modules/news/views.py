3a4619ef0cd32706bd3ff850187701eca99f7b9c
sako <2511013095@qq.com>
2023-05-08 21:42:36 +0800

    用户详情页和详情页的热门排行

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,7 +1,7 @@
-from flask import current_app, jsonify, render_template, abort
+from flask import current_app, jsonify, render_template, abort, session
 
 from . import news_blue
-from ...models import News
+from ...models import News, User
 from ...utils.response_code import RET
 
 
@@ -11,6 +11,17 @@ from ...utils.response_code import RET
 # 返回值: detail.html页面, 用户data字典数据
 @news_blue.route('/<int:news_id>')
 def news_detail(news_id):
+    # 0. 从session中取出用户的user_id
+    user_id = session.get("user_id")
+
+    # 0.1 通过user_id取出用户对象
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
     # 1. 根据新闻编号，查询新闻对象
     try:
         news = News.query.get(news_id)
@@ -22,9 +33,22 @@ def news_detail(news_id):
     if not news:
         abort(408)
 
+    # 3. 获取前6条热门新闻
+    try:
+        click_news = News.query.order_by(News.clicks.desc()).limit(6).all()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 4. 将热门新闻的对象列表，转成字典列表
+    click_news_list = []
+    for news in click_news:
+        click_news_list.append(news.to_dict())
+
     # 2. 携带数据渲染页面
     data = {
-        "news_info": news.to_dict()
+        "news_info": news.to_dict(),
+        "user_info": user.to_dict() if user else "",
+        "news_list": click_news_list
     }
 
     return render_template("news/detail.html", data=data)
