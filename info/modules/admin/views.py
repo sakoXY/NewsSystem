958bec13ed16c9010270d098b5c373b6cb446944
sako <2511013095@qq.com>
2023-05-10 12:40:14 +0800

    管理员后台实现

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,8 +1,55 @@
-from flask import render_template
+from flask import render_template, request, session, redirect, current_app
 
 from . import admin_blue
+from ...models import User
 
 
-@admin_blue.route('/login')
+# 获取/登陆,管理员登陆
+# 请求路径: /admin/login
+# 请求方式: GET,POST
+# 请求参数:GET,无, POST,username,password
+# 返回值: GET渲染login.html页面, POST,login.html页面,errmsg
+@admin_blue.route('/login', methods=["GET", "POST"])
 def admin_login():
-    return render_template("admin/login.html")
+    """
+    1.判断请求方式,如果是GET,直接渲染页面
+    2.如果是POST请求,获取参数
+    3.校验参数,为空校验
+    4.根据用户名取出管理员对象,判断管理员是否存在
+    5.判断管理员的密码是否正确
+    6.管理的session信息记录
+    7.重定向到首页展示
+    :return:
+    """
+    # 1.判断请求方式,如果是GET,直接渲染页面
+    if request.method == "GET":
+        return render_template("admin/login.html")
+
+    # 2.如果是POST请求,获取参数
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    # 3.校验参数,为空校验
+    if not all([username, password]):
+        return render_template("admin/login.html", errmsg="参数不全")
+
+    # 4.根据用户名取出管理员对象,判断管理员是否存在
+    try:
+        admin = User.query.filter(User.mobile == username, User.is_admin == True).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return render_template("admin/login.html", errmsg="用户查询失败")
+
+    if not admin:
+        return render_template("admin/login.html", errmsg="管理员不存在")
+
+    # 5.判断管理员的密码是否正确
+    if not admin.check_password(password):
+        return render_template("admin/login.html", errmsg="密码错误")
+
+    # 6.管理的session信息记录
+    session["user_id"] = admin.id
+    session["is_admin"] = True
+
+    # 7.重定向到首页展示
+    return redirect("http://www.baidu.com")
