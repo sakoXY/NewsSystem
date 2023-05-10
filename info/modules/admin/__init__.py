ed86e0e19eb4b6e796cd747369953978471f102a
sako <2511013095@qq.com>
2023-05-10 14:15:38 +0800

    防止普通用户访问管理员页面的

--- info/modules/admin/__init__.py
+++ info/modules/admin/__init__.py
@@ -1,7 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint, request, session, redirect
 
 # 1.创建管理员蓝图对象
 admin_blue = Blueprint("admin", __name__, url_prefix="/admin")
 
 # 2.装饰视图函数
 from . import views
+
+
+# 使用请求钩子，拦截用户的请求，只有访问了admin_blue所装饰的视图函数需要拦截
+# 1. 拦截的是非登录页面
+# 2. 拦截的是普通用户
+@admin_blue.before_request
+def before_request():
+    """
+    # 1. 判断访问的是否是非登陆页面
+    if request.url.endswith("/admin/login"):
+        pass
+    else:
+        #2. 判断是否是管理员
+        if session.get("is_admin"):
+            pass
+        else:
+            return redirect("/")
+    """
+
+    # 改装上面的代码
+    if not request.url.endswith("/admin/login"):
+        if not session.get("is_admin"):
+            return redirect("/")
